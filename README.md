2c41c9015d9daf2a43a848423c174b1c10b20003
dev33 <antony.hopkins008@gmail.com>
2017-11-28 18:02:38 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+todo-app 
+
+Reactjs practice
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class App extends Component
+{
+    render() {
+        return (
+            <div className="alert alert-primary" role="alert">
+                This is a primary alert—check it out!
+            </div>
+        );
+    }
+}
+
+export default App;
