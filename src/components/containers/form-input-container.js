edd43f6610e7040925d86ab1bbda4a77bcb27df1
dev33 <antony.hopkins008@gmail.com>
2017-12-09 10:10:16 +0200

    check if duplicated tasks exists

--- src/components/containers/form-input-container.js
+++ src/components/containers/form-input-container.js
@@ -4,15 +4,21 @@ import React, {Component} from 'react';
 import FormInput from '../views/form-input';
 import {ItemsList} from "../views/items-list";
 import {addTodo} from '../../actions/todo-actions';
+import some from 'lodash/some'
 
 class FormInputContainer extends Component
 {
     componentDidMount() {
     }
 
-    addItem() {
-        store.dispatch(addTodo(this.refs.inputText.value));
-        this.refs.inputText.value = '';
+    addItem(event) {
+        event.preventDefault();
+        const tasks = store.getState().todos;
+        const taskExists = some(tasks, {text: this.refs.inputText.value});
+        if (!taskExists) {
+            store.dispatch(addTodo(this.refs.inputText.value));
+            this.refs.inputText.value = '';
+        }
     };
 
     render() {
@@ -26,7 +32,6 @@ class FormInputContainer extends Component
 }
 
 const mapStateToProps = function (store) {
-    console.log('mapStateToProps');
     return {
         todos: store.todos
     }
